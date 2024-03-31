20faba623b7491a4f8fdd5c71a77b41af71c0d0a
koudaihirata <2230051@ecc.ac.jp>
2024-04-01 01:40:57 +0900

    悪意のあるコードへの対策

--- work/index.php
+++ work/index.php
@@ -5,7 +5,7 @@
 $n = mt_rand(1,3);
 
 if ($n === 1) {
-  $message = 'エンジニア目指します。' . date('l');
+  $message = '<script>alert(1)</script>エンジニア目指します。' . date('l');
 } elseif ($n === 2) {
   $message = 'プログラミング楽しい！' . date('l');
 } else {
@@ -31,7 +31,7 @@ if ($n === 1) {
       </div>
       <div class="info">
         <h1>平田晃大</h1>
-        <p><?= $message ?></p>
+        <p><?= htmlspecialchars($message ,ENT_QUOTES, 'UTF-8') ?></p>
         <ul>
           <li>
             <a href="https://dotinstall.com" target="_blank">
