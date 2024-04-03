b2b75ff7e2d0255eab5766201abbe2834e9970eb
koudaihirata <2230051@ecc.ac.jp>
2024-04-04 00:10:13 +0900

    配列のキーと値を操作するコードを追加

--- work/BuiltIn/BuiltIn.php
+++ work/BuiltIn/BuiltIn.php
@@ -209,3 +209,25 @@ $newPrices = array_map(
 );
 
 print_r($newPrices);
+echo '<br> <br>';
+
+// 配列のキー､値を操作してみよう
+$scores = [
+    'taguchi' => 80,
+    'hayashi' => 70,
+    'kikuchi' => 60,
+  ];
+  
+  // $keys = array_keys($scores);
+  // print_r($keys);
+  // $values = array_values(($scores));
+  // print_r($values);
+  
+  // if (array_key_exists('taguchi', $scores) === true) {
+  //   echo 'taguchi is here!' . PHP_EOL;
+  // }
+  // if (in_array(80, $scores) === true) {
+  //   echo '80 is here!' . PHP_EOL;
+  // }
+  
+  echo array_search(70, $scores) . PHP_EOL;
