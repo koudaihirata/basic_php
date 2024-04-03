a4e1e77938fa6a67a1498dd81ce1c05172247f50
koudaihirata <2230051@ecc.ac.jp>
2024-04-03 15:24:37 +0900

    sprintf()と関数を使って文字列をフォーマットする*** ***trim()と関数を使って文字列を整形する*** ***マルチバイト文字列を扱う*** ***固定長データを扱う*** ***文字列を検索して置換する*** ***文字列と配列を相互に変換する

--- work/BuiltIn/BuiltIn.php
+++ work/BuiltIn/BuiltIn.php
@@ -10,3 +10,85 @@ $title = 'ビルトイン関数 - ';
 </head>
 
 <?php
+
+// sprintf()を使ってみよう
+$name = 'Apple';
+$score = 32.246;
+
+$info = "[$name][$score]";
+echo $info . PHP_EOL;
+echo '<br>';
+// 文字列だったら %s 、整数だったら %d 、浮動小数点数だったら %f
+// 埋め込みたい値をカンマ区切りで渡せばいい
+$info = sprintf("[%15s][%10.2f]", $name, $score);
+echo $info . PHP_EOL;
+$info = sprintf("[%-15s][%010.2f]", $name, $score);
+echo $info . PHP_EOL;
+echo '<br>';
+printf("[%-15s][%010.2f]" . PHP_EOL, $name, $score);
+echo '<br> <br>';
+
+// 文字列を扱う関数を見ていこう
+$input = ' dot_taguchi  ';
+$input = trim($input);
+echo '<br>';
+//trim() という関数を使ってあげれば前後の空白や改行を除去することができる
+echo strlen($input) . PHP_EOL;
+echo '<br>';
+// $input の中から _ が何番目かを教えてくれる
+echo strpos($input, '_') . PHP_EOL;
+echo '<br>';
+// _を-に変換
+$input = str_replace('_', '-', $input);
+echo $input . PHP_EOL;
+echo '<br> <br>';
+
+// マルチバイト文字列を扱ってみよう
+$input = ' こんにちは  ';
+$input = trim($input);
+// strlen() や strpos() で日本語を扱うにはマルチバイトに対応した別の関数を使う必要があって、 mb_strlen() 、 mb_strpos() を使ってあげる必要があります。
+echo mb_strlen($input) . PHP_EOL; // 5
+echo mb_strpos($input, 'に') . PHP_EOL; // 2
+
+$input = str_replace('にち', 'ばん', $input); // こんばんは
+echo $input . PHP_EOL;
+echo '<br> <br>';
+
+// 固定長データを扱ってみよう
+// substr(文字列, 位置, 桁数)　substr_replace(文字列, 置換文字列, 位置, 桁数)
+$input = '20200320Item-A  1200';
+// Item-AをItem-Bに変更
+$input = substr_replace($input, 'Item-B  ', 8, 8);
+
+$date = substr($input, 0, 8);
+$product = substr($input, 8, 8);
+// $amount = substr($input, 16, 4);
+$amount = substr($input, 16);
+
+echo $date . PHP_EOL;
+echo $product . PHP_EOL;
+echo $amount . PHP_EOL;
+echo number_format($amount) . PHP_EOL;
+echo '<br> <br>';
+
+// 文字列を検索､置換してみよう
+$input = 'Call us at 03-3001-1256 or 03-3015-3222';
+$pattern = '/\d{2}-\d{4}-\d{4}/';
+
+// preg_match($pattern, $input, $matches);
+preg_match_all($pattern, $input, $matches);
+print_r($matches);
+echo '<br>';
+$input = preg_replace($pattern, '**-****-****', $input);
+echo $input . PHP_EOL;$input = 'Call us at 03-3001-1256 or 03-3015-3222';
+echo '<br> <br>';
+
+// 文字列と配列を相互に変換してみよう
+$d = [2020, 11, 15];
+echo "$d[0]-$d[1]-$d[2]" . PHP_EOL;
+echo implode('-', $d) . PHP_EOL;
+echo '<br>';
+$t = '17:32:45';
+print_r(explode(':', $t));
+echo '<br> <br>';
+
