f94b1491b6e77399aa17078880e7491b0dfeae33
koudaihirata <2230051@ecc.ac.jp>
2024-04-05 01:18:37 +0900

    オブジェクトの作成と表示方法の変更

--- work/Object/Object.php
+++ work/Object/Object.php
@@ -25,13 +25,16 @@ class Post
 }
 
 $posts = [];
-$posts[0] = ['text' => 'hello', 'likes' => 0];
-$posts[1] = ['text' => 'hello again', 'likes' => 0];
+// $posts[0] = ['text' => 'hello', 'likes' => 0];
+$posts[0] = new Post();
+$posts[0]->text = 'hello';
+$posts[0]->likes = 0;
+// $posts[1] = ['text' => 'hello again', 'likes' => 0];
+$posts[1] = new Post();
+$posts[1]->text = 'hello again';
+$posts[1]->likes = 0;
 
-function show($post)
-{
-  printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
-}
-
-show($posts[0]);
-show($posts[1]);
+// show($posts[0]);
+// show($posts[1]);
+$posts[0]->show();
+$posts[1]->show();
